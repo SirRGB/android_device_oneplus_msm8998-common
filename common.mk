60e2d3ce6f51bd92664d36309b97d864a5cf9a84
SirRGB <sirrgb@proton.me>
2023-02-27 17:20:53 +0100

    msm8998-common: enable blurs
    * https://github.com/crdroidandroid/android_vendor_crdroid/blob/e1671e3033f762079a212599af891afe43768157/config/crdroid.mk#L38
    
    Signed-off-by: SirRGB <sirrgb@proton.me>

--- common.mk
+++ common.mk
@@ -15,6 +15,8 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
+TARGET_ENABLE_BLUR := true
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
