nkeor/4.14-device_oneplus_msm8998-common

msm8998-common: DeviceSettings: Add possibility to disable charger voltage check
If charger voltage is below 4300V or over 5800V, the kernel stops the
current charging process.

It was noted that sometimes, with kernel 4.14, this is not working
properly as the charging it stopped even if with 4.4 it works fine with
the same charger.

We don't have a kernel fix, but we added the possibility to disable this
check if needed: for this reason, add a flag in DeviceSettings to allow
the user to configure this behaviour and disable this check.

Note that this may be dangerous, so by default the check is enabled.

Co-authored-by: Roberto Sartori <roberto.sartori.android@gmail.com>
Signed-off-by: Namkhai B <me@forkbomb9.ch>

DeviceSettings/src/com/spark/device/DeviceSettings/ChgVoltageCheck.java
@@ -1,0 +1,53 @@
+/*
+* Copyright (C) 2023 Namkhai B.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.spark.device.DeviceSettings;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceManager;
+
+import com.spark.device.DeviceSettings.DeviceSettings;
+
+public class ChgVoltageCheck implements OnPreferenceChangeListener {
+
+    private static final String FILE = "/sys/devices/platform/soc/800f000.qcom,spmi/spmi-0/spmi0-02/800f000.qcom,spmi:qcom,pmi8998@2:qcom,qpnp-smb2/disable_voltage_check";
+
+    public static String getFile() {
+        if (Utils.fileWritable(FILE)) {
+            return FILE;
+        }
+        return null;
+    }
+
+    public static boolean isSupported() {
+        return Utils.fileWritable(getFile());
+    }
+
+    public static boolean isCurrentlyEnabled(Context context) {
+        return Utils.getFileValueAsBoolean(getFile(), false);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Boolean enabled = (Boolean) newValue;
+        Utils.writeValue(getFile(), enabled ? "1" : "0");
+        return true;
+    }
+}

SirRGB/android_device_oneplus_msm8998-common

Signed-off-by: SirRGB <sirrgb@proton.me>

DeviceSettings/src/com/android/device/DeviceSettings/ChgVoltageCheck.java
@@ -1,0 +1,53 @@
+/*
+* Copyright (C) 2023 Namkhai B.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.android.device.DeviceSettings;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceManager;
+
+import com.android.device.DeviceSettings.DeviceSettings;
+
+public class ChgVoltageCheck implements OnPreferenceChangeListener {
+
+    private static final String FILE = "/sys/devices/platform/soc/800f000.qcom,spmi/spmi-0/spmi0-02/800f000.qcom,spmi:qcom,pmi8998@2:qcom,qpnp-smb2/disable_voltage_check";
+
+    public static String getFile() {
+        if (Utils.fileWritable(FILE)) {
+            return FILE;
+        }
+        return null;
+    }
+
+    public static boolean isSupported() {
+        return Utils.fileWritable(getFile());
+    }
+
+    public static boolean isCurrentlyEnabled(Context context) {
+        return Utils.getFileValueAsBoolean(getFile(), false);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Boolean enabled = (Boolean) newValue;
+        Utils.writeValue(getFile(), enabled ? "1" : "0");
+        return true;
+    }
+}