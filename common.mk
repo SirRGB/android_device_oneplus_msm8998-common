381bf679c8ae8fccae08d0e953a614bc0c7a4e50
SirRGB <sirrgb@proton.me>
2025-03-08 22:54:56 +0100

    msm8998-common: bringup rising

--- common.mk
+++ common.mk
@@ -4,6 +4,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Rising
+TARGET_ENABLE_BLUR := true
+RISING_MAINTAINER := SirRGB
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RisingChipset="Qualcomm MSM8998" \
+    RisingMaintainer="SirRGB"
+
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
@@ -16,7 +23,7 @@ PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := true
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-rising
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
