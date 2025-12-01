f629b47d04208cff1ec4a6f40781f026ecbafc18
SirRGB <sirrgb@proton.me>
2025-12-01 22:01:15 +0100

    Revert "msm8998-common: init: Disable serial console"
    This reverts commit be7d5b2ec5c4f7c4848a849d10b520b471b33368.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -31,7 +31,6 @@ TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a73
 
 # Kernel
 BOARD_KERNEL_CMDLINE := \
-    androidboot.console=0 \
     androidboot.hardware=qcom \
     msm_rtb.filter=0x37 \
     ehci-hcd.park=3 \
